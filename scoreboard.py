5d6a715711d19fd421375259e631777ac6182260
federico Lubbert <federico.lubbert@gmail.com>
2024-02-29 21:23:29 +0100

    UPDATE DAY D24

--- scoreboard.py
+++ scoreboard.py
@@ -8,7 +8,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -25,8 +26,10 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def reset(self):
-        if self.score > self.high_score():
-            self.high_score(self.score)
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
 
